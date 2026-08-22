1c6404c757de0a708d4158151ef38022404acd01
Muhammad Muzammil <muzammiltechcanvas@gmail.com>
2026-08-22 08:22:50 -0700

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -100,7 +100,7 @@ function Nav({ user }: { user: IUser }) {
             </div>
 
             <div className="flex flex-col gap-3 font-medium mt-6">
-              {/* <Link href={"/admin/add-grocery"} className="flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 transition-all hover:pl-4 ml-2 mr-2">
+               <Link href={"/admin/add-grocery"} className="flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 transition-all hover:pl-4 ml-2 mr-2">
                 <PlusCircle className="w-5 h-5" /> Add Grocery
               </Link>
               <Link href={"/admin/view-grocery"} className="flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 transition-all hover:pl-4 ml-2 mr-2">
@@ -108,7 +108,7 @@ function Nav({ user }: { user: IUser }) {
               </Link>
               <Link href={"/admin/mange-orders"} className="flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 transition-all hover:pl-4 ml-2 mr-2">
                 <ClipboardCheck className="w-5 h-5" /> Mange Oders
-              </Link> */}
+              </Link> 
               <Link href={"/admin/seller-applications"} className="flex items-center gap-3 p-3 rounded-lg bg-white/10 hover:bg-white/20 transition-all hover:pl-4 ml-2 mr-2">
                 <Store className="w-5 h-5" /> Seller Applications
               </Link>
@@ -223,7 +223,7 @@ function Nav({ user }: { user: IUser }) {
         {user.role == "admin" && (
           <>
             <div className="hidden md:flex items-center gap-4">
-              <Link href={"/admin/add-grocery"} className="flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all">
+              {/* <Link href={"/admin/add-grocery"} className="flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all">
                 <PlusCircle className="w-5 h-5" /> Add Grocery
               </Link>
               <Link href={"/admin/view-grocery"} className="flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all">
@@ -231,7 +231,7 @@ function Nav({ user }: { user: IUser }) {
               </Link>
               <Link href={"/admin/mange-orders"} className="flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all">
                 <ClipboardCheck className="w-5 h-5" /> Mange Oders
-              </Link>
+              </Link> */}
               <Link href={"/admin/bulk-email"} className="flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all">
                 <ClipboardCheck className="w-5 h-5" /> Send Bulk Emails
               </Link>
